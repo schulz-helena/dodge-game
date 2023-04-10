4eee61ed04dcaaf3d23087eb934b43692a5cc105
helschulz <helschulz@uni-potsdam.de>
2023-04-10 12:04:09 +0200

    Add 2 new obstacle types and spawn them in correct positions

--- Assets/Scripts/ObstacleSpawnerBF.cs
+++ Assets/Scripts/ObstacleSpawnerBF.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class ObstacleSpawnerBF : MonoBehaviour
 {
-    public GameObject obstacle;
+    public GameObject[] obstacles;
 
     void Start()
     {
-        int[] positionBiases = {-4, -2, 0, 2, 4};
-        int index = Random.Range(0, 5);
+        int obstacleIndex = Random.Range(0, 3);
+        int index;
+        int[] positionBiases;
+        if (obstacleIndex == 1)
+        {
+            positionBiases = new int[] {-3, -1, 1, 3};
+            index = Random.Range(0, 4);
+        }
+        else 
+        {
+            positionBiases = new int[] {-4, -2, 0, 2, 4};
+            index = Random.Range(0, 5);
+        }
         int bias = positionBiases[index];
         Vector3 obstaclePosition = new Vector3(transform.position.x + bias, transform.position.y, transform.position.z);
-        Instantiate(obstacle, obstaclePosition, transform.rotation);
+        Instantiate(obstacles[obstacleIndex], obstaclePosition, transform.rotation);
     }
 }
